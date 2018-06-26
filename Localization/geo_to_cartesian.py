c9c54cc501ce4e5adc9c5fa624988cbf0501cab9
George Zhang <persensus3551@gmail.com>
2018-06-26 12:56:40 -0400

    [localization]: better display

--- Localization/geo_to_cartesian.py
+++ Localization/geo_to_cartesian.py
@@ -11,24 +11,33 @@ def read_coords(fname='coords.txt', fx=1, fy=1, fr=1):
     max_lat, min_lat = -sys.maxsize, sys.maxsize
     with open(fname, 'r') as file:
         for line in file:
-            long, lat = line.replace('"', '').strip().split('\t')
+            lat, long = line.replace('"', '').strip().split('\t')
             long = fx*float(long)
             lat = fy*float(lat)
-            max_long = max(long, max_long)
-            min_long = min(long, min_long)
-            max_lat = max(lat, max_lat)
-            min_lat = min(lat, min_lat)
             r = fr*0.01
-            coords.append([long, lat, r])
+            max_long = max(long+r, max_long)
+            min_long = min(long-r, min_long)
+            max_lat = max(lat+r, max_lat)
+            min_lat = min(lat-r, min_lat)
+            coords.append([long, lat, None])
 
     L0 = (max_long + min_long) / 2
     phi0 = (max_lat + min_lat) / 2
 
     # print(coords)
+    max_x, min_x = -sys.maxsize, sys.maxsize
+    max_y, min_y = -sys.maxsize, sys.maxsize
     for i, (long, lat, r) in enumerate(coords):
-        long, lat = equirectangular(long, L0, lat, phi0)
-        coords[i][0] = long
-        coords[i][1] = lat
+        x, y = equirectangular(long, L0, lat, phi0)
+        r = fr*0.01
+        coords[i][0] = x
+        coords[i][1] = y
+        coords[i][2] = r
+
+        max_x = max(x + r, max_x)
+        min_x = min(x - r, min_x)
+        max_y = max(y + r, max_y)
+        min_y = min(y - r, min_y)
     # print(coords)
 
     coord_vals = {
@@ -38,7 +47,11 @@ def read_coords(fname='coords.txt', fx=1, fy=1, fr=1):
         'max_long': max_long,
         'max_lat': max_lat,
         'min_long': min_long,
-        'min_lat': min_lat
+        'min_lat': min_lat,
+        'max_x': max_x,
+        'min_x': min_x,
+        'max_y': max_y,
+        'min_y': min_y
     }
 
     return coord_vals
--- Localization/localization.py
+++ Localization/localization.py
@@ -19,10 +19,15 @@ def gen_grid(fx=800, fy=800, fr=50):
     grid = np.zeros((fx+fr, fy+fr))
     return grid
 
-def color_grid(grid, coords):
+def color_grid(grid, coords, scale, min_x, min_y):
     # https://stackoverflow.com/questions/8647024/how-to-apply-a-disc-shaped-mask-to-a-numpy-array
     m, n = grid.shape
     for a, b, r in coords:
+        print(a, b, r)
+        a = int((a-min_x) * scale)
+        b = int((b-min_y) * scale)
+        r = int(r*scale)
+        print(a, b, r)
         y, x = np.ogrid[-a:m-a, -b:n-b]
         mask = x*x + y*y <= r*r
         grid[mask] += 1
@@ -33,10 +38,19 @@ def show_as_image(grid):
     plt.show()
 
 if __name__ == '__main__':
-    coord_vals = read_coords()
+    coord_vals = read_coords(fname='coords2.txt')
     coords = coord_vals['coords']
-    print(coords)
-    # grid = gen_grid()
-    # color_grid(grid, coords)
-    # show_as_image(grid)
+    print(coords,)
+    max_x, min_x = coord_vals['max_x'], coord_vals['min_x']
+    max_y, min_y = coord_vals['max_y'], coord_vals['min_y']
+    print(coord_vals['max_long'], coord_vals['min_long'], coord_vals['max_long'] - coord_vals['min_long'])
+    print(coord_vals['max_lat'], coord_vals['min_lat'], coord_vals['max_lat'] - coord_vals['min_lat'])
+    max_diff = max(max_x - min_x,
+                   max_y - min_y)
+    grid = gen_grid()
+    m, n = grid.shape
+    scale = min(m, n) / max_diff
+    print(scale)
+    color_grid(grid, coords, scale, min_x, min_y)
+    show_as_image(grid)
 
