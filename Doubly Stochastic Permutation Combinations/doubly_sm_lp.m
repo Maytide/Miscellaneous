4def455f2d2a6f8e58b03558dbe07fa8387ad3ca
George Zhang <zhanggq@mcmaster.ca>
2019-02-16 14:51:59 -0500

    updated dspc

--- Doubly Stochastic Permutation Combinations/doubly_sm_lp.m
+++ Doubly Stochastic Permutation Combinations/doubly_sm_lp.m
@@ -4,7 +4,6 @@ clear; close all; clc
 A = [.5 .2 .3;
      .1 .6 .3;
      .4 .2 .4];
-f = 0;
 b = reshape(A', [9 1]);
 b = [b; 1];
 
@@ -12,16 +11,25 @@ id = eye(3);
 v = perms([1 2 3]);
 P = ones(9,6);
 
+% Generate all permutation matrices
 for i=1:6
     p = v(i,:);
     Pi = id(p,:)
     P(:,i) = reshape(Pi',[9 1]);
 end
+% Last row: sum of values = 1
 P = [P; ones(1,6)];
 
-x = linprog([],[],[],P,b)
+% 0 <= xi <= 1
+lb = zeros(6,1); 
+ub = ones(6,1);
+
+% It's a constraint satisfaction problem: no objective function
+% no inequalities
+x = linprog([],[],[],P,b,lb,ub)
 sum(x)
 
+% Verify solution
 X = zeros(3,3);
 for i=1:6
     p = v(i,:);
